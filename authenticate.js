e63a0c8128053cb15e088fcd6157732c26f0f35b
vedha krishna <49089434+yvkrishna@users.noreply.github.com>
2019-11-23 20:52:06 +0530

    Update authenticate.js

--- authenticate.js
+++ authenticate.js
@@ -1,5 +1,5 @@
 var mongojs = require("mongojs");
-var db = mongojs("mongodb://vedha:krishna123@cluster0-shard-00-00-kbuhh.mongodb.net:27017/Hutlabs?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin",["members"]);
+var db = mongojs(connectionString, [collections])
 var express=require("express");
 var bodyParser=require('body-parser');
 const { check, validationResult } = require('express-validator');
