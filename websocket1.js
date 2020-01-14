bcb56009187015ce1736ab99df5fd3285b8f8a2d
vedha krishna <49089434+yvkrishna@users.noreply.github.com>
2020-01-14 19:41:07 +0530

    Update websocket1.js

--- websocket1.js
+++ websocket1.js
@@ -8,4 +8,9 @@ var io=socket(server)
 
 io.on('connection',function(socket){
 	console.log('made socket connection',socket.id);
+
+	socket.on('chat',function(data){
+		io.sockets.emit('chat',data);
+		console.log(data,"hello");
+	});
 })
